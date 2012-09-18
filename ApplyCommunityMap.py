22f9e62a279fc886b689899c37cfcf62567a87bd
Seth Robbins <robbins.sd@gmail.com>
2012-09-18 17:22:40 -0500

    Twelfth Commit

--- ApplyCommunityMap.py
+++ ApplyCommunityMap.py
@@ -3,28 +3,25 @@ import os.path
 import csv
 
 def addInfo(outFile):
-    mapFile=open(r'C:\Users\srobbins\Desktop\OutPut\ETDmap.csv', 'r')
+    mapFile=open(r'C:\Users\srobbins\Desktop\OutPut\ETDmap.csv', 'rb')
     outFile=open(outFile, 'r')
     mapDict=csv.DictReader(mapFile)
     fileDict=csv.DictReader(outFile)        
-    testOutput=open(r'C:\Users\srobbins\Desktop\OutPut\mapTest.csv', 'w')
+    testOutput=open(r'C:\Users\srobbins\Desktop\OutPut\mapTest.csv', 'wb')
     fieldnames=fileDict.fieldnames
     fieldnames.append("collection")
     fieldnames.append("notes")
-    print fieldnames
     testDict=csv.DictWriter(testOutput, fieldnames)
     testDict.writeheader()
     for fileRow in fileDict:
         mapFile.seek(0)
         for mapRow in mapDict:
             length=len(fileRow['department name'])
-            print mapRow['Discipline']
             if fileRow['department name']==mapRow['Total Records: 3584'][:length]:
                 fileRow['collection']=mapRow['Collection Handle']
-                print fileRow['collection']+"="+mapRow['Collection Handle']
-                fileRow['notes']=mapRow['Notes']
-                print "writing "+str(fileRow)
+                fileRow['notes']=mapRow['Notes'].strip()
                 testDict.writerow(fileRow)
+
 
     
     
